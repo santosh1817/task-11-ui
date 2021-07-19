3fcfdd180531476bf6995e28288f076bceafac67
santosh1817 <santosh01994@gmail.com>
2021-07-19 08:37:35 +0530

    first

---
+++ src/redux/reducers/dictionaryData.js
@@ -0,0 +1,29 @@
+import { FETCH_ALL, POST_DATA, SEARCH_WORD } from '../actions/types';
+
+const initialState = {
+  allData: [],
+};
+const dictionaryData = (state = initialState, action) => {
+  console.log(action, 'act');
+  switch (action.type) {
+    case FETCH_ALL:
+      return {
+        ...state,
+        allData: action.allData,
+      };
+    case POST_DATA:
+      return {
+        ...state,
+
+        allData: [...state.allData, action.addedWord],
+      };
+    case SEARCH_WORD:
+      return {
+        ...state,
+        allData: action.allData,
+      };
+    default:
+      return state;
+  }
+};
+export default dictionaryData;
